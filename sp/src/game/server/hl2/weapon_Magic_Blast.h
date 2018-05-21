d62fd8974ec122dcd0167f61f538dc40cfbaaa02
Lord-Scrubington-II <39203491+Lord-Scrubington-II@users.noreply.github.com>
2018-05-21 15:01:24 -0700

    Update and rename weapon_Magic_Blast.h to grenade_Magic_Blast.h

--- sp/src/game/server/hl2/weapon_Magic_Blast.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRENADE_FRAG_H
-#define GRENADE_FRAG_H
-#pragma once
-
-class CBaseGrenade;
-struct edict_t;
-
-CBaseGrenade *Fraggrenade_Create( const Vector &position, const QAngle &angles, const Vector &velocity, const AngularImpulse &angVelocity, CBaseEntity *pOwner, float timer, bool combineSpawned );
