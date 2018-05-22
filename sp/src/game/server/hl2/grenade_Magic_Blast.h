aeaef30bb467227491ff39bea971b6659f5a022c
Clay Tsay <claytsay@users.noreply.github.com>
2018-05-22 15:00:39 -0700

    Migration: 221459U MAY 18
    - Removed the grenade_Magic_Blast.h and grenade_Magic_Blast.cpp

--- sp/src/game/server/hl2/grenade_Magic_Blast.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRENADE_MAGIC_H
-#define GRENADE_MAGIC_H
-#pragma once
-
-class MagicBlast;
-struct edict_t;
-
-MagicBlast *Fraggrenade_Create( const Vector &position, const QAngle &angles, const Vector &velocity, const AngularImpulse &angVelocity, CBaseEntity *pOwner, float timer);
