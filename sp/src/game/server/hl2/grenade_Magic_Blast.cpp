47d31a1fdecf8babeb510a00f4d607fc926b4aaa
Lord-Scrubington-II <39203491+Lord-Scrubington-II@users.noreply.github.com>
2018-05-24 14:50:48 -0700

    Magic Blast Test-Ready
    It's a bugbait that makes an explosion. See if it functions

--- sp/src/game/server/hl2/grenade_Magic_Blast.cpp
+++ sp/src/game/server/hl2/grenade_Magic_Blast.cpp
@@ -1,413 +1,235 @@
 #include "cbase.h"
-#include "basegrenade_shared.h"
+#include "player.h"
+#include "ammodef.h"
+#include "gamerules.h"
 #include "grenade_Magic_Blast.h"
-#include "Sprite.h"
-#include "SpriteTrail.h"
+#include "weapon_brickbat.h"
 #include "soundent.h"
+#include "decals.h"
+#include "fire.h"
+#include "shake.h"
+#include "ndebugoverlay.h"
+#include "vstdlib/random.h"
+#include "engine/IEngineSound.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-#define FRAG_GRENADE_GRACE_TIME_AFTER_PICKUP 1.5f
-#define FRAG_GRENADE_WARN_TIME 1.5f
+extern short	g_sModelIndexFireball;
 
-const float GRENADE_COEFFICIENT_OF_RESTITUTION = 0.2f;
+extern ConVar    sk_plr_dmg_Magic_Blast;
+extern ConVar    sk_npc_dmg_Magic_Blast;
+ConVar    sk_Magic_Blast_radius("sk_Magic_Blast_radius", "0");
 
-ConVar sk_plr_dmg_fraggrenade	( "sk_plr_dmg_fraggrenade","0");
-ConVar sk_npc_dmg_fraggrenade	( "sk_npc_dmg_fraggrenade","0");
-ConVar sk_fraggrenade_radius	( "sk_fraggrenade_radius", "0");
+#define Magic_Blast_EXPLOSION_VOLUME	1024
 
-#define GRENADE_MODEL "models/Weapons/w_grenade.mdl"
+BEGIN_DATADESC(CGrenade_Magic_Blast)
 
-class CGrenadeFrag : public CBaseGrenade
-{
-	DECLARE_CLASS( CGrenadeFrag, CBaseGrenade );
+DEFINE_FIELD(m_pFireTrail, FIELD_CLASSPTR),
 
-#if !defined( CLIENT_DLL )
-	DECLARE_DATADESC();
-#endif
-					
-	~CGrenadeFrag( void );
-
-public:
-	void	Spawn( void );
-	void	OnRestore( void );
-	void	Precache( void );
-	bool	CreateVPhysics( void );
-	void	CreateEffects( void );
-	void	SetTimer( float detonateDelay, float warnDelay );
-	void	SetVelocity( const Vector &velocity, const AngularImpulse &angVelocity );
-	int		OnTakeDamage( const CTakeDamageInfo &inputInfo );
-	void	DelayThink();
-	void	VPhysicsUpdate( IPhysicsObject *pPhysics );
-
-	// this function only used in episodic.
-#if defined(HL2_EPISODIC) && 0 // FIXME: HandleInteraction() is no longer called now that base grenade derives from CBaseAnimating
-	bool	HandleInteraction(int interactionType, void *data, CBaseCombatCharacter* sourceEnt);
-#endif 
-
-	void	InputSetTimer( inputdata_t &inputdata );
-
-protected:
-	CHandle<CSprite>		m_pMainGlow;
-	CHandle<CSpriteTrail>	m_pGlowTrail;
-
-	float	m_flNextBlipTime;
-	bool	m_inSolid;
-};
-
-LINK_ENTITY_TO_CLASS( npc_grenade_frag, CGrenadeFrag );
-
-BEGIN_DATADESC( CGrenadeFrag )
-
-	// Fields
-	DEFINE_FIELD( m_pMainGlow, FIELD_EHANDLE ),
-	DEFINE_FIELD( m_pGlowTrail, FIELD_EHANDLE ),
-	DEFINE_FIELD( m_flNextBlipTime, FIELD_TIME ),
-	DEFINE_FIELD( m_inSolid, FIELD_BOOLEAN ),
-	
-	// Function Pointers
-	DEFINE_THINKFUNC( DelayThink ),
-
-	// Inputs
-	DEFINE_INPUTFUNC( FIELD_FLOAT, "SetTimer", InputSetTimer ),
+// Function Pointers
+DEFINE_FUNCTION(Magic_BlastTouch),
+DEFINE_FUNCTION(Magic_BlastThink),
 
 END_DATADESC()
 
+LINK_ENTITY_TO_CLASS(grenade_Magic_Blast, CGrenade_Magic_Blast);
+
+void CGrenade_Magic_Blast::Spawn(void)
+{
+	SetMoveType(MOVETYPE_FLYGRAVITY, MOVECOLLIDE_FLY_BOUNCE);
+	SetSolid(SOLID_BBOX);
+	SetCollisionGroup(COLLISION_GROUP_PROJECTILE);
+
+	SetModel("models/weapons/w_bugbait.mdl");
+
+	UTIL_SetSize(this, Vector(-6, -6, -2), Vector(6, 6, 2));
+
+	SetTouch(Magic_BlastTouch);
+	SetThink(Magic_BlastThink);
+	SetNextThink(gpGlobals->curtime + 0.1f);
+
+	m_flDamage = sk_plr_dmg_Magic_Blast.GetFloat();
+	m_DmgRadius = sk_Magic_Blast_radius.GetFloat();
+
+	m_takedamage = DAMAGE_YES;
+	m_iHealth = 1;
+
+	SetGravity(1.0);
+	SetFriction(0.8);  // Give a little bounce so can flatten
+	SetSequence(1);
+
+	m_pFireTrail = SmokeTrail::CreateSmokeTrail();
+
+	if (m_pFireTrail)
+	{
+		m_pFireTrail->m_SpawnRate = 48;
+		m_pFireTrail->m_ParticleLifetime = 1.0f;
+
+		m_pFireTrail->m_StartColor.Init(0.2f, 0.2f, 0.2f);
+		m_pFireTrail->m_EndColor.Init(0.0, 0.0, 0.0);
+
+		m_pFireTrail->m_StartSize = 8;
+		m_pFireTrail->m_EndSize = 32;
+		m_pFireTrail->m_SpawnRadius = 4;
+		m_pFireTrail->m_MinSpeed = 8;
+		m_pFireTrail->m_MaxSpeed = 16;
+		m_pFireTrail->m_Opacity = 0.25f;
+
+		m_pFireTrail->SetLifetime(20.0f);
+		m_pFireTrail->FollowEntity(this, "0");
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Input  :
+// Output :
+//-----------------------------------------------------------------------------
+void CGrenade_Magic_Blast::Magic_BlastTouch(CBaseEntity *pOther)
+{
+	Detonate();
+}
 
 //-----------------------------------------------------------------------------
 // Purpose: 
+//
+//
 //-----------------------------------------------------------------------------
-CGrenadeFrag::~CGrenadeFrag( void )
+void CGrenade_Magic_Blast::Detonate(void)
 {
-}
-
-void CGrenadeFrag::Spawn( void )
-{
-	Precache( );
-
-	SetModel( GRENADE_MODEL );
-
-	if( GetOwnerEntity() && GetOwnerEntity()->IsPlayer() )
-	{
-		m_flDamage		= sk_plr_dmg_fraggrenade.GetFloat() - 20;
-		m_DmgRadius		= sk_fraggrenade_radius.GetFloat() + 200;
-	}
-	else
-	{
-		m_flDamage		= sk_npc_dmg_fraggrenade.GetFloat();
-		m_DmgRadius		= sk_fraggrenade_radius.GetFloat();
-	}
-
-	SetSize( -Vector(4,4,4), Vector(4,4,4) );
-	SetCollisionGroup( COLLISION_GROUP_WEAPON );
-	CreateVPhysics();
-
-
-	AddSolidFlags( FSOLID_NOT_STANDABLE );
-	
-
-	BaseClass::Spawn();
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CGrenadeFrag::OnRestore( void )
-{
-	// If we were primed and ready to detonate, put FX on us.
-	if (m_flDetonateTime > 0)
-		CreateEffects();
-
-	BaseClass::OnRestore();
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CGrenadeFrag::CreateEffects( void )
-{
-	// Start up the eye glow
-	m_pMainGlow = CSprite::SpriteCreate( "sprites/redglow1.vmt", GetLocalOrigin(), false );
-
-	int	nAttachment = LookupAttachment( "fuse" );
-
-	if ( m_pMainGlow != NULL )
-	{
-		m_pMainGlow->FollowEntity( this );
-		m_pMainGlow->SetAttachment( this, nAttachment );
-		m_pMainGlow->SetTransparency( kRenderGlow, 255, 255, 255, 200, kRenderFxNoDissipation );
-		m_pMainGlow->SetScale( 0.2f );
-		m_pMainGlow->SetGlowProxySize( 4.0f );
-	}
-
-}
-
-bool CGrenadeFrag::CreateVPhysics()
-{
-	// Create the object in the physics system
-	VPhysicsInitNormal( SOLID_BBOX, 0, false );
-	return true;
-}
-
-// this will hit only things that are in newCollisionGroup, but NOT in collisionGroupAlreadyChecked
-class CTraceFilterCollisionGroupDelta : public CTraceFilterEntitiesOnly
-{
-public:
-	// It does have a base, but we'll never network anything below here..
-	DECLARE_CLASS_NOBASE( CTraceFilterCollisionGroupDelta );
-	
-	CTraceFilterCollisionGroupDelta( const IHandleEntity *passentity, int collisionGroupAlreadyChecked, int newCollisionGroup )
-		: m_pPassEnt(passentity), m_collisionGroupAlreadyChecked( collisionGroupAlreadyChecked ), m_newCollisionGroup( newCollisionGroup )
-	{
-	}
-	
-	virtual bool ShouldHitEntity( IHandleEntity *pHandleEntity, int contentsMask )
-	{
-		if ( !PassServerEntityFilter( pHandleEntity, m_pPassEnt ) )
-			return false;
-		CBaseEntity *pEntity = EntityFromEntityHandle( pHandleEntity );
-
-		if ( pEntity )
-		{
-			if ( g_pGameRules->ShouldCollide( m_collisionGroupAlreadyChecked, pEntity->GetCollisionGroup() ) )
-				return false;
-			if ( g_pGameRules->ShouldCollide( m_newCollisionGroup, pEntity->GetCollisionGroup() ) )
-				return true;
-		}
-
-		return false;
-	}
-
-protected:
-	const IHandleEntity *m_pPassEnt;
-	int		m_collisionGroupAlreadyChecked;
-	int		m_newCollisionGroup;
-};
-/*
-void CGrenadeFrag::VPhysicsUpdate( IPhysicsObject *pPhysics )
-{
-	BaseClass::VPhysicsUpdate( pPhysics );
-	Vector vel;
-	AngularImpulse angVel;
-	pPhysics->GetVelocity( &vel, &angVel );
-	
-	Vector start = GetAbsOrigin();
-	// find all entities that my collision group wouldn't hit, but COLLISION_GROUP_NONE would and bounce off of them as a ray cast
-	CTraceFilterCollisionGroupDelta filter( this, GetCollisionGroup(), COLLISION_GROUP_NONE );
-	trace_t tr;
-
-	// UNDONE: Hull won't work with hitboxes - hits outer hull.  But the whole point of this test is to hit hitboxes.
-#if 0
-	UTIL_TraceHull( start, start + vel * gpGlobals->frametime, CollisionProp()->OBBMins(), CollisionProp()->OBBMaxs(), CONTENTS_HITBOX|CONTENTS_MONSTER|CONTENTS_SOLID, &filter, &tr );
-#else
-	UTIL_TraceLine( start, start + vel * gpGlobals->frametime, CONTENTS_HITBOX|CONTENTS_MONSTER|CONTENTS_SOLID, &filter, &tr );
-#endif
-	if ( tr.startsolid )
-	{
-		if ( !m_inSolid )
-		{
-			// UNDONE: Do a better contact solution that uses relative velocity?
-			vel *= -GRENADE_COEFFICIENT_OF_RESTITUTION; // bounce backwards
-			pPhysics->SetVelocity( &vel, NULL );
-		}
-		m_inSolid = true;
-		return;
-	}
-	m_inSolid = false;
-	if ( tr.DidHit() )
-	{
-		Vector dir = vel;
-		VectorNormalize(dir);
-		// send a tiny amount of damage so the character will react to getting bonked
-		CTakeDamageInfo info( this, GetThrower(), pPhysics->GetMass() * vel, GetAbsOrigin(), 0.1f, DMG_CRUSH );
-		tr.m_pEnt->TakeDamage( info );
-
-		// reflect velocity around normal
-		vel = -2.0f * tr.plane.normal * DotProduct(vel,tr.plane.normal) + vel;
-		
-		// absorb 80% in impact
-		vel *= GRENADE_COEFFICIENT_OF_RESTITUTION;
-		angVel *= -0.5f;
-		pPhysics->SetVelocity( &vel, &angVel );
-	}
-}
-*/
-
-//-----------------------------------------------------------------------------
-// The actual explosion 
-//-----------------------------------------------------------------------------
-void CGrenadeFrag::DoExplosion( void )
-{
-	// Explode
-	ExplosionCreate( GetAbsOrigin(), GetAbsAngles(), GetOwnerEntity(), GetDamage(), CGrenadeFrag::EXPLOSION_RADIUS, 
-		SF_ENVEXPLOSION_NOSPARKS | SF_ENVEXPLOSION_NODLIGHTS | SF_ENVEXPLOSION_NOSMOKE, 0.0f, this);
-}
-
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-//-----------------------------------------------------------------------------
-void CGrenadeFrag::Explode( void )
-{
-	// Don't explode against the skybox. Just pretend that 
-	// the missile flies off into the distance.
-	Vector forward;
-
-	GetVectors( &forward, NULL, NULL );
-
-	trace_t tr;
-	UTIL_TraceLine( GetAbsOrigin(), GetAbsOrigin() + forward * 16, MASK_SHOT, this, COLLISION_GROUP_NONE, &tr );
+	SetModelName(NULL_STRING);		//invisible
+	AddSolidFlags(FSOLID_NOT_SOLID);	// intangible
 
 	m_takedamage = DAMAGE_NO;
-	SetSolid( SOLID_NONE );
-	if( tr.fraction == 1.0 || !(tr.surface.flags & SURF_SKY) )
+
+	trace_t trace;
+	UTIL_TraceLine(GetAbsOrigin(), GetAbsOrigin() + Vector(0, 0, -128), MASK_SOLID_BRUSHONLY,
+		this, COLLISION_GROUP_NONE, &trace);
+
+	// Pull out of the wall a bit
+	if (trace.fraction != 1.0)
 	{
-		DoExplosion();
+		SetLocalOrigin(trace.endpos + (trace.plane.normal * (m_flDamage - 24) * 0.6));
 	}
 
-	if( m_hRocketTrail )
+	int contents = UTIL_PointContents(GetAbsOrigin());
+
+	if ((contents & MASK_WATER))
 	{
-		m_hRocketTrail->SetLifetime(0.1f);
-		m_hRocketTrail = NULL;
-	}
-
-	if ( m_hOwner != NULL )
-	{
-		m_hOwner->NotifyRocketDied();
-		m_hOwner = NULL;
-	}
-
-	StopSound( "Missile.Ignite" );
-	UTIL_Remove( this );
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: 
-// Input  : *pOther - 
-//----------------------------------------------------------------------------
-
-void CGrenadeFrag::BlastTouch( CBaseEntity *pOther )
-{
-	Assert( pOther );
-	
-	// Don't touch triggers (but DO hit weapons)
-	if ( pOther->IsSolidFlagSet(FSOLID_TRIGGER|FSOLID_VOLUME_CONTENTS) && pOther->GetCollisionGroup() != COLLISION_GROUP_WEAPON )
-	{
-		// Some NPCs are triggers that can take damage (like antlion grubs). We should hit them.
-		if ( ( pOther->m_takedamage == DAMAGE_NO ) || ( pOther->m_takedamage == DAMAGE_EVENTS_ONLY ) )
-			return;
-	}
-
-	Explode();
-}
-
-
-void CGrenadeFrag::Precache( void )
-{
-	PrecacheModel( GRENADE_MODEL );
-
-	PrecacheScriptSound( "Grenade.Blip" );
-
-	PrecacheModel( "sprites/redglow1.vmt" );
-	PrecacheModel( "sprites/bluelaser1.vmt" );
-
-	BaseClass::Precache();
-}
-
-
-void CGrenadeFrag::DelayThink() 
-{
-	if( gpGlobals->curtime > m_flDetonateTime )
-	{
-		Detonate();
+		UTIL_Remove(this);
 		return;
 	}
 
-	if( !m_bHasWarnedAI && gpGlobals->curtime >= m_flWarnAITime )
+	EmitSound("Grenade_Molotov.Detonate");
+
+	// Start some fires
+	int i;
+	QAngle vecTraceAngles;
+	Vector vecTraceDir;
+	trace_t firetrace;
+
+	for (i = 0; i < 16; i++)
 	{
-#if !defined( CLIENT_DLL )
-		CSoundEnt::InsertSound ( SOUND_DANGER, GetAbsOrigin(), 400, 1.5, this );
-#endif
-		m_bHasWarnedAI = true;
-	}
-	
-	if( gpGlobals->curtime > m_flNextBlipTime )
-	{
-		BlipSound();
-		
-		if( m_bHasWarnedAI )
+		// build a little ray
+		vecTraceAngles[PITCH] = random->RandomFloat(45, 135);
+		vecTraceAngles[YAW] = random->RandomFloat(0, 360);
+		vecTraceAngles[ROLL] = 0.0f;
+
+		AngleVectors(vecTraceAngles, &vecTraceDir);
+
+		Vector vecStart, vecEnd;
+
+		vecStart = GetAbsOrigin() + (trace.plane.normal * 128);
+		vecEnd = vecStart + vecTraceDir * 512;
+
+		UTIL_TraceLine(vecStart, vecEnd, MASK_SOLID_BRUSHONLY, this, COLLISION_GROUP_NONE, &firetrace);
+
+		Vector	ofsDir = (firetrace.endpos - GetAbsOrigin());
+		float	offset = VectorNormalize(ofsDir);
+
+		if (offset > 128)
+			offset = 128;
+
+		//Get our scale based on distance
+		float scale = 0.1f + (0.75f * (1.0f - (offset / 128.0f)));
+		float growth = 0.1f + (0.75f * (offset / 128.0f));
+
+		if (firetrace.fraction != 1.0)
 		{
-			m_flNextBlipTime = gpGlobals->curtime + FRAG_GRENADE_BLIP_FAST_FREQUENCY;
+			FireSystem_StartFire(firetrace.endpos, scale, growth, 30.0f, (SF_FIRE_START_ON | SF_FIRE_SMOKELESS | SF_FIRE_NO_GLOW), (CBaseEntity*) this, FIRE_NATURAL);
 		}
-		else
+	}
+	// End Start some fires
+
+	CPASFilter filter2(trace.endpos);
+
+	te->Explosion(filter2, 0.0,
+		&trace.endpos,
+		g_sModelIndexFireball,
+		2.0,
+		15,
+		TE_EXPLFLAG_NOPARTICLES,
+		m_DmgRadius,
+		m_flDamage);
+
+	CBaseEntity *pOwner;
+	pOwner = GetOwnerEntity();
+	SetOwnerEntity(NULL); // can't traceline attack owner if this is set
+
+	UTIL_DecalTrace(&trace, "Scorch");
+
+	UTIL_ScreenShake(GetAbsOrigin(), 25.0, 150.0, 1.0, 750, SHAKE_START);
+	CSoundEnt::InsertSound(SOUND_DANGER, GetAbsOrigin(), BASEGRENADE_EXPLOSION_VOLUME, 3.0);
+
+	RadiusDamage(CTakeDamageInfo(this, pOwner, m_flDamage, DMG_BLAST), GetAbsOrigin(), m_DmgRadius, CLASS_NONE, NULL);
+
+	AddEffects(EF_NODRAW);
+	SetAbsVelocity(vec3_origin);
+	SetNextThink(gpGlobals->curtime + 0.2);
+
+	if (m_pFireTrail)
+	{
+		UTIL_Remove(m_pFireTrail);
+	}
+
+	UTIL_Remove(this);
+}
+
+//------------------------------------------------------------------------------
+// Purpose :
+// Input   :
+// Output  :
+//------------------------------------------------------------------------------
+void CGrenade_Magic_Blast::Magic_BlastThink(void)
+{
+	// See if I can lose my owner (has dropper moved out of way?)
+	// Want do this so owner can throw the brickbat
+	if (GetOwnerEntity())
+	{
+		trace_t tr;
+		Vector	vUpABit = GetAbsOrigin();
+		vUpABit.z += 5.0;
+
+		CBaseEntity* saveOwner = GetOwnerEntity();
+		SetOwnerEntity(NULL);
+		UTIL_TraceEntity(this, GetAbsOrigin(), vUpABit, MASK_SOLID, &tr);
+		if (tr.startsolid || tr.fraction != 1.0)
 		{
-			m_flNextBlipTime = gpGlobals->curtime + FRAG_GRENADE_BLIP_FREQUENCY;
+			SetOwnerEntity(saveOwner);
 		}
 	}
-
-	SetNextThink( gpGlobals->curtime + 0.1 );
+	SetNextThink(gpGlobals->curtime + 0.1f);
 }
 
-void CGrenadeFrag::SetVelocity( const Vector &velocity, const AngularImpulse &angVelocity )
+void CGrenade_Magic_Blast::Precache(void)
 {
-	IPhysicsObject *pPhysicsObject = VPhysicsGetObject();
-	if ( pPhysicsObject )
-	{
-		pPhysicsObject->AddVelocity( &velocity, &angVelocity );
-	}
+	BaseClass::Precache();
+
+	PrecacheModel("models/weapons/w_bugbait.mdl");
+
+	UTIL_PrecacheOther("_firesmoke");
+
+	PrecacheScriptSound("Grenade_Magic_Blast.Detonate");
 }
 
-
-#if defined(HL2_EPISODIC) && 0 // FIXME: HandleInteraction() is no longer called now that base grenade derives from CBaseAnimating
-extern int	g_interactionBarnacleVictimGrab; ///< usually declared in ai_interactions.h but no reason to haul all of that in here.
-extern int g_interactionBarnacleVictimBite;
-extern int g_interactionBarnacleVictimReleased;
-
-bool CGrenadeFrag::HandleInteraction(int interactionType, void *data, CBaseCombatCharacter* sourceEnt)
-{
-	// allow fragnades to be grabbed by barnacles. 
-	if ( interactionType == g_interactionBarnacleVictimGrab )
-	{
-		// give the grenade another five seconds seconds so the player can have the satisfaction of blowing up the barnacle with it
-		float timer = m_flDetonateTime - gpGlobals->curtime + 5.0f;
-		SetTimer( timer, timer - FRAG_GRENADE_WARN_TIME );
-
-		return true;
-	}
-	else if ( interactionType == g_interactionBarnacleVictimBite )
-	{
-		// detonate the grenade immediately 
-		SetTimer( 0, 0 );
-		return true;
-	}
-	else if ( interactionType == g_interactionBarnacleVictimReleased )
-	{
-		// take the five seconds back off the timer.
-		float timer = MAX(m_flDetonateTime - gpGlobals->curtime - 5.0f,0.0f);
-		SetTimer( timer, timer - FRAG_GRENADE_WARN_TIME );
-		return true;
-	}
-	else
-	{
-		return BaseClass::HandleInteraction( interactionType, data, sourceEnt );
-	}
-}
-#endif
-
-void CGrenadeFrag::InputSetTimer( inputdata_t &inputdata )
-{
-	SetTimer( inputdata.value.Float(), inputdata.value.Float() - FRAG_GRENADE_WARN_TIME );
-}
-
-CBaseGrenade *Fraggrenade_Create( const Vector &position, const QAngle &angles, const Vector &velocity, const AngularImpulse &angVelocity, CBaseEntity *pOwner, float timer, bool combineSpawned )
-{
-	// Don't set the owner here, or the player can't interact with grenades he's thrown
-	CGrenadeFrag *pGrenade = (CGrenadeFrag *)CBaseEntity::Create( "npc_grenade_frag", position, angles, pOwner );
-	
-	pGrenade->SetVelocity( velocity, angVelocity );
-	pGrenade->SetThrower( ToBaseCombatCharacter( pOwner ) );
-	pGrenade->m_takedamage = DAMAGE_EVENTS_ONLY;
-
-	return pGrenade;
-}
